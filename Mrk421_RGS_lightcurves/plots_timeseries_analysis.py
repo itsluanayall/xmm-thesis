8e1faa1d428059ad216dd5c0e52a302c68fda2ce
luana modafferi <luana.modafferi@outlook.com>
2020-05-23 18:44:26 +0200

    total lc subplots

--- Mrk421_RGS_lightcurves/plots_timeseries_analysis.py
+++ Mrk421_RGS_lightcurves/plots_timeseries_analysis.py
@@ -48,45 +48,266 @@ def plot(x, y, title, xlabel, ylabel, output_folder, dy, dx=[]):
 def plot_total_lc(x, y, title, xlabel, ylabel, output_folder, dy, dx=[]):
     """
     """
+    plt.errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)  
+    plt.show()
 
-
-    fig = plt.figure(figsize=(80,10))
-    ax = fig.add_subplot(1, 1, 1)
-    #plt.autoscale()
-    #ratio = 0.2
-    #ax.set_aspect(1.0/ax.get_data_ratio()*ratio)
-    #plt.axis([51330, 59040, 0, 60])
+    fig, axs = plt.subplots(3,11, sharey=True, figsize=(30,12), gridspec_kw = {'wspace':0.2, 'hspace':0.2})
+    fig.suptitle(title, fontsize=20, y=0.92)
     #Drop NaN values by making a numpy mask
     mask_nan = np.invert(np.isnan(y)) 
     x = x[mask_nan]
     y = y[mask_nan]
     dy = dy[mask_nan]
 
-    if len(dx)==0:
-        
-        plt.errorbar(x,y, yerr=dy, color='r', marker='.', ecolor='gray', linestyle='',)
+    # Subplot 1
+    axs[0,0].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)  
+    axs[0,0].grid(True)
+    axs[0,0].set_xlim(51683,51697)
+    axs[0,0].annotate('Year 2000', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5, -5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[0,0].margins(0)
+
+    # Subplot 2
+    axs[0,1].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[0,1].grid(True)
+    axs[0,1].set_xlim(51850,51863)
+    axs[0,1].margins(0)
+
+    #Subplot 3
+    axs[0,2].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[0,2].grid(True)
+    axs[0,2].set_xlim(52035,52040)
+    axs[0,2].annotate('Year 2001', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[0,2].margins(0)
+
     
-    else:
+    #Subplot 4
+    axs[0,3].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[0,3].grid(True)
+    axs[0,3].set_xlim(52393,52400)
+    axs[0,3].margins(0) 
+    axs[0,3].get_xaxis().get_major_formatter().set_useOffset(False)
+    axs[0,3].annotate('Year 2002', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
 
-        dx = dx[mask_nan]
-        plt.errorbar(x,y, yerr=dy, xerr=dx, color='black', marker='.', markersize=0.05, ecolor='gray', linestyle='-')
-    plt.yscale('log')
-    plt.grid(True)
-    plt.title(title, fontsize=20)
-    plt.xlabel(xlabel, fontsize=10)
-    plt.ylabel(ylabel, fontsize=10)
-    plt.xlim(51840,51870)
-    plt.xticks(fontsize=10)
-    plt.yticks(fontsize=10)
-    plt.margins(0)
 
-    # Separate in years
-    year = 2001
-    for mjd in [51910, 52275, 52640, 53005, 53371, 	53736, 54101, 54466,54832, 55197, 55562, 55927, 56293, 56658, 57023, 57388, 57754, 58119, 58484]: 
-        plt.text(x=mjd+70, y=57, s=year)
-        year+=1
-    plt.vlines([51910, 52275, 52640, 53005, 53371, 	53736, 54101, 54466,54832, 55197, 55562, 55927, 56293, 56658, 57023, 57388, 57754, 58119, 58484], min(y), max(y), colors='r', linestyles='solid')
- 
+    #Subplot 5
+    axs[0,4].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[0,4].grid(True)
+    axs[0,4].set_xlim(52581,52612)
+    axs[0,4].margins(0)
+
+    #Subplot 6
+    axs[0,5].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[0,5].grid(True)
+    axs[0,5].set_xlim(52789,52800)
+    axs[0,5].margins(0)
+    axs[0,5].annotate('Year 2003', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[0,5].get_xaxis().get_major_formatter().set_useOffset(False)
+    
+    #Subplot 7
+    axs[0,6].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[0,6].grid(True)
+    axs[0,6].set_xlim(52956,52985)
+    axs[0,6].margins(0)
+
+    
+    #Subplot 8
+    axs[0,7].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[0,7].grid(True)
+    axs[0,7].annotate('Year 2004', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[0,7].set_xlim(53125,53136)
+    axs[0,7].margins(0)
+
+    #Subplot 9
+    axs[0,8].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[0,8].grid(True)
+    axs[0,8].annotate('Year 2005', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[0,8].set_xlim(53678,53690)
+    axs[0,8].margins(0)
+
+    #Subplot 10
+    axs[0,9].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[0,9].grid(True)
+    axs[0,9].annotate('Year 2006', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[0,9].set_xlim(53854,53884)
+    axs[0,9].margins(0)
+
+    #Subplot 11
+    axs[0,10].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[0,10].grid(True)
+    axs[0,10].set_xlim(54068, 54080)
+    axs[0,10].margins(0)
+    
+
+    #Subplot 12
+    axs[1,0].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[1,0].grid(True)
+    axs[1,0].annotate('Year 2007', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[1,0].set_xlim(54220, 54235)
+    axs[1,0].set_ylabel(ylabel, fontsize=14)
+    axs[1,0].margins(0)
+
+
+    #Subplot 13
+    axs[1,1].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[1,1].grid(True)
+    axs[1,1].set_xlim(54418,54430)
+    axs[1,1].margins(0)
+
+
+    #Subplot 14
+    axs[1,2].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[1,2].grid(True)
+    axs[1,2].annotate('Year 2008', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[1,2].set_xlim(54592,54618)
+    axs[1,2].margins(0)
+
+    #Subplot 15
+    axs[1,3].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[1,3].grid(True)
+    axs[1,3].set_xlim(54785,54800)
+    axs[1,3].margins(0)
+
+    #Subplot 16
+    axs[1,4].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[1,4].grid(True)
+    axs[1,4].annotate('Year 2009', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[1,4].set_xlim(54965, 54980)
+    axs[1,4].margins(0)
+
+    #Subplot 17
+    axs[1,5].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[1,5].grid(True)
+    axs[1,5].set_xlim(55145, 55160)
+    axs[1,5].margins(0)
+
+    #Subplot 18
+    axs[1,6].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[1,6].grid(True)
+    axs[1,6].annotate('Year 2010', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[1,6].set_xlim(55310, 55325)
+    axs[1,6].margins(0)
+
+    #Subplot 19
+    axs[1,7].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[1,7].grid(True)
+    axs[1,7].set_xlim(55505, 55520)
+    axs[1,7].margins(0)
+
+    #Subplot 20
+    axs[1,8].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[1,8].grid(True)
+    axs[1,8].annotate('Year 2011', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[1,8].set_xlim(55694, 55708)
+    axs[1,8].get_xaxis().get_major_formatter().set_useOffset(False)
+    axs[1,8].margins(0)
+
+    #Subplot 21
+    axs[1,9].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[1,9].grid(True)
+    axs[1,9].set_xlim(55873,55901)
+    axs[1,9].margins(0)
+
+    #Subplot 22
+    axs[1,10].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[1,10].grid(True)
+    axs[1,10].annotate('Year 2014', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[1,10].set_xlim(56770, 56785)
+    axs[1,10].margins(0)
+
+    #Subplot 23
+    axs[2,0].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[2,0].grid(True)
+    axs[2,0].annotate('Year 2015', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[2,0].set_xlim(57175, 57190)
+    axs[2,0].margins(0)
+    
+
+    #Subplot 24
+    axs[2,1].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[2,1].grid(True)
+    axs[2,1].set_xlim(57328, 57340)
+    axs[2,1].margins(0)
+
+    #Subplot 25
+    axs[2,2].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[2,2].grid(True)
+    axs[2,2].set_xlim(57360, 57375)
+    axs[2,2].margins(0)
+
+    #Subplot 26
+    axs[2,3].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[2,3].grid(True)
+    axs[2,3].annotate('Year 2016', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[2,3].set_xlim(57513, 57535)
+    axs[2,3].margins(0)
+
+    #Subplot 27
+    axs[2,4].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[2,4].grid(True)
+    axs[2,4].set_xlim(57692, 57707)
+    axs[2,4].get_xaxis().get_major_formatter().set_useOffset(False)
+    axs[2,4].margins(0)
+
+    #Subplot 28
+    axs[2,5].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[2,5].grid(True)
+    axs[2,5].annotate('Year 2017', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r') 
+    axs[2,5].set_xlim(57870, 57885)
+    axs[2,5].margins(0)
+
+    #Subplot 29
+    axs[2,6].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[2,6].grid(True)
+    axs[2,6].set_xlim(58069, 58080)
+    axs[2,6].margins(0)
+
+
+    #Subplot 30
+    axs[2,7].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[2,7].grid(True)
+    axs[2,7].annotate('Year 2018', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[2,7].set_xlim(58235, 58250)
+    axs[2,7].margins(0)
+
+    #Subplot 31
+    axs[2,8].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[2,8].grid(True)
+    axs[2,8].set_xlim(58438, 58449)
+    axs[2,8].get_xaxis().get_major_formatter().set_useOffset(False)
+    axs[2,8].margins(0)
+
+    #Subplot 32
+    axs[2,9].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[2,9].grid(True)
+    axs[2,9].annotate('Year 2019', xy=(0, 1), xycoords='axes fraction', fontsize=8, xytext=(5,-5), textcoords='offset points',
+                horizontalalignment='left', verticalalignment='top', color='r')
+    axs[2,9].set_xlim(58603, 58612)
+    axs[2,9].margins(0)
+
+    #Subplot 33
+    axs[2,10].errorbar(x,y, yerr=dy, color='b', marker='.', ecolor='gray', linestyle='', markersize=0.5)
+    axs[2,10].grid(True)
+    axs[2,10].set_xlabel(xlabel, fontsize=14)
+    axs[2,10].set_xlim(58812, 58822)
+    axs[2,10].margins(0)
+
     #Save figure 
     plt.savefig(f'{output_folder}/{title}.png')
     plt.show()
