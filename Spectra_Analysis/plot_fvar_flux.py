8eaf69f134888e899e50b748d055c07d2f951485
luana modafferi <luana.modafferi@outlook.com>
2020-06-22 16:41:36 +0200

    filter on fvar != -1

--- Spectra_Analysis/plot_fvar_flux.py
+++ Spectra_Analysis/plot_fvar_flux.py
@@ -2,7 +2,7 @@ import os
 import matplotlib.pyplot as plt
 from config import CONFIG
 from astropy.table import Table
-
+import pandas as pd
 
 missing_xsa_obs = [658802001]
 
@@ -17,27 +17,39 @@ if __name__ == "__main__":
     hdul_lc = Table.read(os.path.join(products_dir, "RGS_Lightcurves", "obs_table.fits"), hdu=1)   
     data_lc = hdul_lc.to_pandas()
     data_lc = data_lc.dropna(subset=['RGS_Rate'])   #do not consider NaN Rates
+    print(len(data_lc))
 
     #Read Flux table for logpar and powerlaw
     hdul_spec = Table.read(os.path.join(products_dir, "RGS_Spectra", "spectra_table.fits"), hdu=1)
     data_spec = hdul_spec.to_pandas()
     data_spec = data_spec[data_spec['tbinid'] == 0]   #Use only average spectra
     data_spec_clean = data_spec[data_spec['obsid'] != 658802001]
-
+    data_spec_clean = data_spec[data_spec['obsid'] != 411082701]
     data_spec_zlogp = data_spec_clean[data_spec_clean['model']=='constant*TBabs*zlogp']
     data_spec_zpowe = data_spec_clean[data_spec_clean['model']=='constant*TBabs*zpowe']
+    print(len(data_spec_zlogp))
 
     #Plot panel
     fig, axs =plt.subplots(2, 1, figsize=(20,10), gridspec_kw={'hspace':0.4})
     #fig.suptitle(f'Correlation Fractional Variability and Flux', fontsize=15)
 
+    # make error bars from upper and lower limits
     flux_top_logpar = data_spec_zlogp['flux_up'].values - data_spec_zlogp['flux'].values
     flux_bot_logpar = data_spec_zlogp['flux'].values - data_spec_zlogp['flux_low'].values 
     flux_top_pow = data_spec_zpowe['flux_up'].values - data_spec_zpowe['flux'].values
     flux_bot_pow = data_spec_zpowe['flux'].values - data_spec_zpowe['flux_low'].values 
 
-    axs[0].errorbar(data_spec_zlogp['flux'].values, data_lc['F_var'].values, yerr=data_lc['F_var_sigma'].values, xerr=(flux_bot_logpar, flux_top_logpar), color='b', linestyle='', marker='.', markersize=0.5, ecolor='b', label='zlogpar')
-    axs[0].errorbar(data_spec_zpowe['flux'].values, data_lc['F_var'].values, yerr=data_lc['F_var_sigma'].values, xerr=(flux_bot_pow, flux_top_pow), color='red', linestyle='', marker='.', markersize=0.5, ecolor='red', label='zpowerlaw')
+    #Create dataframes for plot
+    fvar_flux_logpar = pd.DataFrame({'flux': data_spec_zlogp['flux'].values, 'fvar': data_lc['F_var'].values, 'fvar_err': data_lc['F_var_sigma'].values, 'xerr_bot': flux_bot_logpar, 'xerr_top':flux_top_logpar})
+    fvar_flux_pow = pd.DataFrame({'flux': data_spec_zpowe['flux'].values, 'fvar': data_lc['F_var'].values, 'fvar_err': data_lc['F_var_sigma'].values, 'xerr_bot': flux_bot_pow, 'xerr_top':flux_top_pow})
+    fvar_flux_logpar = fvar_flux_logpar[fvar_flux_logpar['fvar']!=-1.]
+    fvar_flux_pow = fvar_flux_pow[fvar_flux_pow['fvar']!= -1.]
+
+    #axs[0].errorbar(data_spec_zlogp['flux'].values, data_lc['F_var'].values, yerr=data_lc['F_var_sigma'].values, xerr=(flux_bot_logpar, flux_top_logpar), color='b', linestyle='', marker='.', markersize=0.5, ecolor='b', label='zlogpar')
+    #axs[0].errorbar(data_spec_zpowe['flux'].values, data_lc['F_var'].values, yerr=data_lc['F_var_sigma'].values, xerr=(flux_bot_pow, flux_top_pow), color='red', linestyle='', marker='.', markersize=0.5, ecolor='red', label='zpowerlaw')
+    axs[0].errorbar(data=fvar_flux_logpar, x='flux', y='fvar', yerr='fvar_err', xerr=('xerr_bot', 'xerr_top'), color='b', linestyle='', marker='.', markersize=0.5, ecolor='b', label='zlogpar')
+    axs[0].errorbar(data=fvar_flux_pow, x='flux', y='fvar', yerr='fvar_err', xerr=('xerr_bot', 'xerr_top'), color='b', linestyle='', marker='.', markersize=0.5, ecolor='b', label='zpowerlaw')
+
     axs[0].grid(True)
     axs[0].set_ylabel('$F_{var}$', fontsize=15)
     axs[0].set_xlabel('Flux [cm$^{-2}$ erg s$^{-1}$]', fontsize=15)
