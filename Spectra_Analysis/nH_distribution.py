65e6ab49cd0e49fd6e88b6eda074bfc657f3760f
luana modafferi <luana.modafferi@outlook.com>
2020-08-03 10:41:46 +0200

    finished nH distribution

--- Spectra_Analysis/nH_distribution.py
+++ Spectra_Analysis/nH_distribution.py
@@ -30,11 +30,11 @@ if __name__ == "__main__":
     nH_top_powerlaw = data_spec_zpowe['nH_up'].values - data_spec_zpowe['nH'].values
     nH_bot_powerlaw = data_spec_zpowe['nH'].values - data_spec_zpowe['nH_low'].values
 
-
-    g = (sns.jointplot("nH_logpar", "nH_powerlaw", data=data_distrib, space=0, color="royalblue", marker='.').plot_joint(sns.kdeplot,zorder=0, n_levels=5))
+    
+    g = (sns.jointplot("nH_logpar", "nH_powerlaw", data=data_distrib, space=0, height=7, color="royalblue", marker='.').plot_joint(sns.kdeplot,zorder=0, n_levels=5))
     plt.errorbar(data_distrib.nH_logpar, data_distrib.nH_powerlaw, linestyle='', ecolor='lightsteelblue', alpha=0.4, xerr=(nH_bot_logpar, nH_top_logpar), yerr=(nH_bot_powerlaw, nH_top_powerlaw))
-    plt.errorbar(x=0.0192, y=0.0192, marker='+', markersize=10)
-    plt.errorbar(x=0.0079, y=0.0142, marker='x', color='b', markersize=10)
+    plt.errorbar(x=0.0145, y=0.0145, marker='+', markersize=10, markeredgewidth=1, markeredgecolor='r') 
+    plt.errorbar(x=0.0079, y=0.0142, marker='x', color='b', markersize=7)
 
     plt.xlabel('nH_logpar [$10^{22}$ cm$^-2$]')
     plt.ylabel('nH_power [$10^{22}$ cm$^-2$]')
@@ -43,12 +43,16 @@ if __name__ == "__main__":
     counts_pw, bins_pw, _ = plt.hist(data_distrib['nH_powerlaw'], bins=20)
     plt.title('nH powerlaw')
     bin_centers_pw = 0.5 * (bins_pw[:-1] + bins_pw[1:])
+    binsize_pw = (bins_pw[1]-bins_pw[0])/2
+    print(binsize_pw)
     print('most frequent nH value for powerlaw:', bin_centers_pw[np.argmax(counts_pw)])
 
     fig3 = plt.figure()
     counts_lp, bins_lp, _ = plt.hist(data_distrib['nH_logpar'], bins=20)
     plt.title('nH logparabola')
     bin_centers_lp = 0.5 * (bins_lp[:-1] + bins_lp[1:])
+    binsize_lp = (bins_lp[1]-bins_lp[0])/2
+    print(binsize_lp)
     print('most frequent nH value for logparabola:', bin_centers_lp[np.argmax(counts_lp)])
 
     '''
